1c4f3ea6224149c9fd57ed24a65744aa8b6695d3
Opolskiy <opolskiy@bpcbt.com>
2017-12-25 20:12:49 +0300

    Init commit.

---
+++ src/main/java/QuadraticEquation.java
@@ -0,0 +1,36 @@
+public class QuadraticEquation {
+
+    private float coefA;
+    private float coefB;
+    private float coefC;
+
+    public QuadraticEquation(float coefA, float coefB, float coefC) {
+        this.coefA = coefA;
+        this.coefB = coefB;
+        this.coefC = coefC;
+    }
+
+    public float getCoefA() {
+        return coefA;
+    }
+
+    public float getCoefB() {
+        return coefB;
+    }
+
+    public float getCoefC() {
+        return coefC;
+    }
+
+    public float getDiscriminant() {
+        float discriminant = 0;
+
+        return discriminant;
+    }
+
+    public float[] getRoots(){
+        float [] roots = {};
+
+        return roots;
+    }
+}
---
+++ src/test/java/QuadraticEquationTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class QuadraticEquationTest {
+    @Test
+    public void getDiscriminant() throws Exception {
+    }
+
+    @Test
+    public void getRoots() throws Exception {
+    }
+
+}
